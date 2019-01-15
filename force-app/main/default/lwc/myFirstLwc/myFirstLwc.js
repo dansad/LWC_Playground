5e84436c145ae37991e216044bd3e2232f1c627a
Daniel <Daniel.Sadavoy@investorsgroup.com>
2019-01-15 09:21:52 -0500

    add project files

---
+++ force-app/main/default/lwc/myFirstLwc/myFirstLwc.js
@@ -0,0 +1,13 @@
+import { LightningElement, track } from 'lwc';
+
+export default class HelloConditionalRendering extends LightningElement {
+    @track areDetailsVisible = false;
+
+    handleChange(event) {
+        this.areDetailsVisible = event.target.checked;
+    }
+
+    bringLazy(event){
+        event.target.label = "Good";
+    }
+}
